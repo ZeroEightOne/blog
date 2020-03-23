8728dad63e99a8ff57e5cfa570965280cee0bc7c
ZeroEightOne <amnajowen@hotmail.com>
2020-03-23 23:05:16 +0700

    it should be hashrouter or something i don't know

--- src/index.js
+++ src/index.js
@@ -1,18 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Route, Link, BrowserRouter as Router } from 'react-router-dom'
+import { HashRouter, Switch } from "react-router-dom"
 import './index.css';
 import App from './App';
 
 import templatePage from "./posts/template";
 
 const routing = (
-    <Router>
-        <div>
+    <HashRouter>
+        <Switch>
             <Route exact path = "/" component = { App } />
             <Route path = "/template" component = { templatePage } />
-        </div>
-    </Router>
+        </Switch>
+    </HashRouter>
 )
 
 
